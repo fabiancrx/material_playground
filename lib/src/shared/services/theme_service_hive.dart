632055aea8449bbed68e472c58121eef3a985c14
Fabian <fquintanar@cpsproducts.com>
2025-04-10 12:52:54 -0400

    Fix issues for flutter 3.29

--- lib/src/shared/services/theme_service_hive.dart
+++ lib/src/shared/services/theme_service_hive.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_ce/hive.dart';
 
 import '../utils/app_data_dir/app_data_dir.dart';
 import 'theme_service.dart';
