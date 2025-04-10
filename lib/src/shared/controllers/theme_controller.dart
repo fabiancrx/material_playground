18ba4c25608ac31af55a2f1d29fb166155f77d24
Fabian <fquintanar@cpsproducts.com>
2025-04-10 12:13:28 -0400

    Fix issues for flutter 3.29

--- lib/src/shared/controllers/theme_controller.dart
+++ lib/src/shared/controllers/theme_controller.dart
@@ -1525,7 +1525,7 @@ class ThemeController with ChangeNotifier {
       setBottomNavBarMuteUnselected(true, false);
       // NavigationBar settings
       setNavBarIndicatorSchemeColor(SchemeColor.secondary, false);
-      setNavBarBackgroundSchemeColor(SchemeColor.surfaceVariant, false);
+      // setNavBarBackgroundSchemeColor(SchemeColor.surfaceVariant, false);
       setNavBarSelectedIconSchemeColor(SchemeColor.onSurface, false);
       setNavBarSelectedLabelSchemeColor(SchemeColor.onSurface, false);
       setNavBarUnselectedSchemeColor(SchemeColor.onSurface, false);
@@ -1610,18 +1610,18 @@ class ThemeController with ChangeNotifier {
       setDrawerIndicatorSchemeColor(SchemeColor.primary, false);
       // BottomNavigationBar
       setBottomNavBarMuteUnselected(false, false);
-      setBottomNavBarBackgroundSchemeColor(SchemeColor.surfaceVariant, false);
+      // setBottomNavBarBackgroundSchemeColor(SchemeColor.surfaceVariant, false);
       // NavigationBar settings
-      setNavBarBackgroundSchemeColor(SchemeColor.background, false);
+      // setNavBarBackgroundSchemeColor(SchemeColor.background, false);
       setNavBarMuteUnselected(false, false);
-      setNavBarSelectedIconSchemeColor(SchemeColor.background, false);
+      // setNavBarSelectedIconSchemeColor(SchemeColor.background, false);
       setNavBarSelectedLabelSchemeColor(SchemeColor.primary, false);
       setNavBarElevation(1, false);
       setNavBarIndicatorSchemeColor(SchemeColor.primary, false);
       setNavBarIndicatorOpacity(1.0, false);
       // NavigationRail settings
       setNavRailMuteUnselected(false, false);
-      setNavRailSelectedIconSchemeColor(SchemeColor.background, false);
+      // setNavRailSelectedIconSchemeColor(SchemeColor.background, false);
       setNavRailSelectedLabelSchemeColor(SchemeColor.primary, false);
       setNavRailIndicatorSchemeColor(SchemeColor.primary, false);
       setNavRailIndicatorOpacity(1.0, false);
@@ -1684,7 +1684,7 @@ class ThemeController with ChangeNotifier {
       // BottomNavigationBar
       setBottomNavBarMuteUnselected(false, false);
       // NavigationBar settings
-      setNavBarBackgroundSchemeColor(SchemeColor.background, false);
+      // setNavBarBackgroundSchemeColor(SchemeColor.background, falsse);
       setNavBarMuteUnselected(false, false);
       setNavBarSelectedIconSchemeColor(SchemeColor.onPrimary, false);
       setNavBarSelectedLabelSchemeColor(SchemeColor.primary, false);
